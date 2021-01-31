1e7144ffad894c41ae3670abde16e4959c10d79e
Patrick Paiva <patrickpaiva88@gmail.com>
2021-01-31 09:43:31 -0300

    added total price preview

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,6 +64,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disableInfo}
+                    price={this.state.totalPrice}
                 />
             </>
         )
