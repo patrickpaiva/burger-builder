ba1b7aa391bd00dd089ac2f53dc9180ab51fb1a5
Patrick Paiva <patrickpaiva88@gmail.com>
2021-01-31 10:50:34 -0300

    added showing and hidding modal logic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients) {
@@ -72,6 +73,11 @@ class BurgerBuilder extends Component {
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
         this.updatePurchaseState(updatedIngredients)
     }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    }
+
     render() {
         const disableInfo = {
             ...this.state.ingredients
@@ -82,7 +88,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -91,6 +97,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disableInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice}
                 />
             </>
