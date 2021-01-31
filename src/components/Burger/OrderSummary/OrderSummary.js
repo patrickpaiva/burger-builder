470291ea48d4f8d53c509adc9d549bc021b50bae
Patrick Paiva <patrickpaiva88@gmail.com>
2021-01-31 11:14:04 -0300

    added total price to modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checktout?</p>
             <Button
                 btnType="Danger"
