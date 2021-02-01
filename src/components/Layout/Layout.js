ea80ca3e5f561f97f900615f88bda42d2a0e0e20
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-01 11:19:38 -0300

    added toggle button for SideDrawer menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../hoc/Auxi'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
@@ -6,14 +6,34 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 import classes from './Layout.module.css'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar clicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+
+        )
+
+    }
+}
+
+export default Layout
