12e0d1130ebfc20042bbc126fe5d097521d86676
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-02 10:12:11 -0300

    added logic to store purchase at firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -83,7 +84,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!')
+        // alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Patrick',
+                address: {
+                    street: 'Paia 1',
+                    zipCode: '3123121',
+                    country: 'Brazil'
+                },
+                email: 'teste@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => console.error(error))
     }
 
     render() {
