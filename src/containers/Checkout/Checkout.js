fa099a25dc5f41fcd03bc29237de1dc057922a0f
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-05 11:03:57 -0300

    created Contact Data component and added navigation to it

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
     state = {
@@ -38,6 +40,7 @@ class Checkout extends Component {
                 CheckoutCancelled={this.checkoutCancelledHandler}
                 CheckoutContinued={this.checkoutContinuedHandler}
                 />
+                <Route path={`${this.props.match.path}/contact-data`} component={ContactData} />
             </div>
         )
     }
