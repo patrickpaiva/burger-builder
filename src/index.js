ea33dbfe4ca7850c13ad959c6609857ad8ae397d
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-09 11:11:09 -0300

    added redux-thunk and applied it as middleware to the store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import {createStore} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 
 import './index.css';
@@ -10,7 +11,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 
-const store = createStore(burgerBuilderReducer)
+const store = createStore(burgerBuilderReducer, applyMiddleware(thunk))
 
 const app = (
   <Provider store={store}>
