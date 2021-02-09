bb633a6e47e258edc39d7dc9f2352aafa00bf735
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-09 17:51:43 -0300

    created global variable for server key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart())
         const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
             password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD7_7NkGsXF9XbSgZo8ZCXoDlAm2ReEM10', authData)
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`
+        if (!isSignup) {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response)
                 dispatch(authSuccess(response.data))
