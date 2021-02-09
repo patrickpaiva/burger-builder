cee76497dec0440c4ef7745088e17ebc94eb9f1f
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-09 14:52:32 -0300

    fixed contact data page to redirect when no cheking out

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
@@ -34,16 +34,25 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data')
     }
     render() {
+        let summary = <Redirect to="/" />
+
+        if (this.props.ings) {
+            summary = (
+                <>
+                    <CheckoutSummary 
+                        ingredients={this.props.ings}
+                        CheckoutCancelled={this.checkoutCancelledHandler}
+                        CheckoutContinued={this.checkoutContinuedHandler}
+                    />
+                    <Route 
+                        path={`${this.props.match.path}/contact-data`} 
+                        component={ContactData} />
+                </>
+            )
+        }
         return (
             <div>
-                <CheckoutSummary 
-                    ingredients={this.props.ings}
-                    CheckoutCancelled={this.checkoutCancelledHandler}
-                    CheckoutContinued={this.checkoutContinuedHandler}
-                />
-                <Route 
-                    path={`${this.props.match.path}/contact-data`} 
-                    component={ContactData} />
+                {summary}
             </div>
         )
     }
