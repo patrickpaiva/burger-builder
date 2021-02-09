98fc47c0a520232df7d83622b68e75df6fa7417f
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-09 18:34:40 -0300

    added logic to token protect some pages

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -22,10 +22,10 @@ export const purchaseBurgerStart = () => {
     }
 }
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart())
-        axios.post('/orders.json', orderData)
+        axios.post(`/orders.json?auth=${token}`, orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData))
             })
@@ -62,10 +62,10 @@ export const fecthOrdersStart = () => {
     }
 }
 
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
     return dispatch => {
         dispatch(fecthOrdersStart())
-        axios.get('/orders.json')
+        axios.get(`/orders.json?auth=${token}`)
             .then(response => {
                 const fetchedOrders = []
                 for (let key in response.data) {
