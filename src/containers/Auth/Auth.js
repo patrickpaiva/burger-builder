dcfa7ce1f8fd030435c10fc8a824e453d202fc74
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-10 15:21:24 -0300

    shared check validity with Auth and ContactData

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.module.css'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import { checkValidity } from '../../shared/utility'
 
 import * as actions from '../../store/actions/index'
 
@@ -44,35 +45,6 @@ class Auth extends Component {
         isSignup: true
     }
 
-    checkValidity(value, rules) {
-        let isValid = true
-        if (!rules) {
-            return true
-        }
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid
-    }
 
     componentDidMount() {
         if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
@@ -86,7 +58,7 @@ class Auth extends Component {
             [controlName]: {
                 ...this.state.controls[controlName],
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
             }
         }
