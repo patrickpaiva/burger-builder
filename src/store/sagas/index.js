23e77691965dd1460c9f3439a7637b92027298f2
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-12 11:55:49 -0300

    added examples of call, takeLatest and all

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, all, takeLatest } from 'redux-saga/effects'
 
 import * as actionTypes from '../actions/actionTypes'
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth'
@@ -6,14 +6,16 @@ import { purchaseBurgerSaga, fetchedOrdersSaga } from './order'
 import { initIngredientsSaga } from './burgerBuilder'
 
 export function* watchAuth() {
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    yield all([
+        takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    ])
 }
 
 export function* watchOrder() {
-    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga)
+    yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga)
     yield takeEvery(actionTypes.FETCH_ORDERS_INIT, fetchedOrdersSaga)
 
 }
