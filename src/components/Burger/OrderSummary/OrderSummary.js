b1ffd13153f22d9471cbb04f735de287824efa7a
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-16 16:29:46 -0300

    updated Modal with React memo and changed to React FC Modal and OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Button from '../../UI/Button/Button'
 
-class orderSummary extends Component {
-    // componentWillUpdate() {
-    //     console.log('[OrderSummary.js] Will Update')
-    // }
-    render() {
-        const ingredientSummary = Object.keys(this.props.ingredients)
-            .map(item => {
-                return <li key={item}><span style={{ textTransform: 'capitalize' }}>{item}</span>: {this.props.ingredients[item]}</li>
-            })
-        return (<>
-            <h3>Your Order</h3>
-            <p>A delicius burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checktout?</p>
-            <Button
-                btnType="Danger"
-                clicked={this.props.purchaseCancelled}
-            >CANCEL</Button>
-            <Button
-                btnType="Success"
-                clicked={this.props.purchaseContinued}
-            >CONTINUE</Button>
-        </>)
-    }
+const orderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(item => {
+            return <li key={item}><span style={{ textTransform: 'capitalize' }}>{item}</span>: {props.ingredients[item]}</li>
+        })
+    return (<>
+        <h3>Your Order</h3>
+        <p>A delicius burger with the following ingredients:</p>
+        <ul>
+            {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checktout?</p>
+        <Button
+            btnType="Danger"
+            clicked={props.purchaseCancelled}
+        >CANCEL</Button>
+        <Button
+            btnType="Success"
+            clicked={props.purchaseContinued}
+        >CONTINUE</Button>
+    </>)
+
 }
 
 export default orderSummary
